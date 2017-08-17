fa2e35eb1d52cd36354df9273fec0a1cf5dfe27f
svbuddha <sunitha.buddha@gmail.com>
2017-08-16 19:19:34 -0500

    test case 23 and 26 successfully passed

--- ManyFunctions.java
+++ ManyFunctions.java
@@ -73,7 +73,7 @@ public class ManyFunctions
      */
     public static boolean isVowel(char a)
     {
-        if( a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' ) {
+        if( a == 'a' || a == 'e' || a == 'i' || a == 'o' || a == 'u' || a == 'A' || a == 'E' || a == 'I' || a == 'O' || a == 'U' ) {
             return true;
         } else {
             return false;
