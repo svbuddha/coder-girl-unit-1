deb769d0b5813b828ceaecd693abf0d12b5ef0d8
svbuddha <sunitha.buddha@gmail.com>
2017-09-06 19:04:37 -0500

    condition to max number is added

--- MultiplicationTables.java
+++ MultiplicationTables.java
@@ -3,25 +3,28 @@ import java.util.Scanner;
 
 public class MultiplicationTablesGrid
 {
- public static void main(String[]args)
- {
-     Scanner keyboard = new Scanner (System.in);
-     out.println("Welcome to Multiplicaion Tables");
-     out.println("");
-     out.print("How large would you like to see them? ");
-     int number = keyboard.nextInt();
-     out.println("");
-     out.print("    ");
-     for (int row = 0; row <= number;row++)
-         out.printf("%4d",row );
-         out.println();
+    public static void main(String[]args)
+    {
+        Scanner keyboard = new Scanner (System.in);
+        out.println("Welcome to Multiplicaion Tables");
+        out.println("");
+        out.print("How large would you like to see them ? ");
+        int number = keyboard.nextInt();
+        if (number<=20) {
+            out.print("    ");
+            for (int row = 0; row <= number;row++)
+                out.printf("%4d",row );
+                out.println();
+                for ( int col=0; col <= number; col++){
+                    out.printf("%4d",col );
+                    for(int row=0; row <= number; row++)
+                        out.printf("%4d",col * row);
+                        out.println();
+            }
+        } else {
+            out.println("Error, enter number less than 20");
 
-     for ( int col=0; col <= number; col++){
-           out.printf("%4d",col );
-           for(int row=0; row <= number; row++){
-               out.printf("%4d",col * row);
-           }
-           out.println();
-       }
-   }
+        }
+    }
 }
+
